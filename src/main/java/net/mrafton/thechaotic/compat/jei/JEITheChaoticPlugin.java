d48a8ebcc46b99ec4ed336aea0f8e103e1955b93
MrAfton <fabianherzmann03@gmail.com>
2025-11-21 14:43:41 +0100

    Updated to neoforge 21.1.215
    Signed-off-by: MrAfton <fabianherzmann03@gmail.com>

--- src/main/java/net/mrafton/thechaotic/compat/jei/JEITheChaoticPlugin.java
+++ src/main/java/net/mrafton/thechaotic/compat/jei/JEITheChaoticPlugin.java
@@ -3,13 +3,16 @@ package net.mrafton.thechaotic.compat.jei;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.mrafton.thechaotic.TheChaotic;
+import net.mrafton.thechaotic.block.ModBlocks;
 import net.mrafton.thechaotic.recipe.ChronoCrafterRecipe;
 import net.mrafton.thechaotic.recipe.ModRecipes;
 import net.mrafton.thechaotic.screen.machine.ChronoCrafterScreen;
@@ -37,10 +40,14 @@ public class JEITheChaoticPlugin implements IModPlugin {
         registration.addRecipes(ChronoCrafterRecipeCategory.CHRONO_CRAFTER_RECIPE_RECIPE_TYPE,chronoCrafterRecipes);
     }
 
-
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
         registration.addRecipeClickArea(ChronoCrafterScreen.class,70,30,25,20,
                 ChronoCrafterRecipeCategory.CHRONO_CRAFTER_RECIPE_RECIPE_TYPE);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(ModBlocks.CHRONO_CRAFTER,ChronoCrafterRecipeCategory.CHRONO_CRAFTER_RECIPE_RECIPE_TYPE);
     }
 }
